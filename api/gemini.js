aa51ada774c17ac9d4dc608aaa415f3ae135656e
tehaupooo24-maker <tehaupooo24@gmail.com>
2026-06-18 16:59:33 +0200

    Update gemini.js

--- api/gemini.js
+++ api/gemini.js
@@ -1,3 +1,4 @@
+// v2
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', 'https://aquaduo.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
